67af22b956c8c339618a2f244922a5e3733a9a78
Gerardo Garcia <38366630+garcia116@users.noreply.github.com>
2022-12-02 18:23:55 -0800

    Update AverageOfSubarrayOfSizeK.java

--- 1 - Sliding Window/AverageOfSubarrayOfSizeK.java
+++ 1 - Sliding Window/AverageOfSubarrayOfSizeK.java
@@ -14,3 +14,29 @@ Here is the final output containing the averages of all  subarrays of size '5':
 
 Output: [2.2, 2.8, 2.4, 3.6, 2.8]
 */
+
+import java.util.Arrays;
+
+public class AverageOfSubarrayOfSizeK {
+    public static double[] findAverages(int k, int[] arr){
+        double[] result = new double[arr.length - k + 1];
+        double windowSum = 0;
+        int windowStart = 0;
+        
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++){
+            windowSum += arr[windowEnd]; // add next element
+            // slide window, checking boundaries
+            if(windowEnd >= k - 1){
+                result[windowStart] = windowSum / k;
+                windowSum -= arr[windowStart];
+                windowStart++;
+            }
+        }
+        return result;
+    }
+    public static void main(String args[]) {
+         double[] result = AverageOfSubarrayOfSizeK.findAverages(5,
+         new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+         System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+  }
+}
